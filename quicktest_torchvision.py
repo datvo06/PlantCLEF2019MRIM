9e81fabb731bb5404c6b685f016534ddf20dc6d2
TheEngineer <datvo06@gmail.com>
2019-03-09 18:15:38 +0100

    Add random rotation

--- quicktest_torchvision.py
+++ quicktest_torchvision.py
@@ -192,6 +192,7 @@ print(model_ft)
 data_transforms = {
     'train': transforms.Compose([
         transforms.RandomResizedCrop(input_size),
+        transforms.RandomRotation((0, 360)),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
