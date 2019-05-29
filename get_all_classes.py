7be3b8806827cd6f4e7d760e32835979ded4cfe1
Thanh-dat Nguyen <nguyetha@decore2.imag.fr>
2019-05-29 21:09:47 +0200

    Adding family and genus for get all classes

--- get_all_classes.py
+++ get_all_classes.py
@@ -7,13 +7,13 @@ from bs4 import BeautifulSoup
 # Get the tags
 # write all to excel file
 if __name__ == '__main__':
-    base_path = '/video/clef/LifeCLEF/PlantCLEF2019/train/store_temp_small_class/'
+    base_path = '/video/clef/LifeCLEF/PlantCLEF2019/train/data/'
     dirlist = filter(
             lambda dirname: os.path.isdir(os.path.join(base_path, dirname)),
             os.listdir(base_path))
-    with open('list_class_small_names.csv', 'w') as csvfile:
+    with open('list_class_names.csv', 'w') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(['ID', 'Species Name'])
+        writer.writerow(['ID', 'Species Name', 'Genus', 'Family Name'])
         for directory in dirlist:
             # Get dir name
             dirname = os.path.split(directory)[1]
@@ -25,13 +25,21 @@ if __name__ == '__main__':
                     lambda filename: filename.endswith('xml'),
                     os.listdir(dir_path))
             list_unique_names = []
+            list_unique_family = []
+            list_unique_genus = []
             for fname in filelist:
                 fpath = os.path.join(dir_path, fname)
                 with open(fpath, "r") as xmlfile:
                     xml_text = xmlfile.read()
                     soup = BeautifulSoup(xml_text, 'lxml')
                     species = soup.find(lambda tag: tag.name.lower() == "species")
+                    genus = soup.find(lambda tag: tag.name.lower() == "genus")
+                    family = soup.find(lambda tag: tag.name.lower() == "family")
                     list_unique_names.append(species.text)
+                    list_unique_family.append(family.text)
+                    list_unique_genus.append(genus.text)
             list_unique_names = list(set(list_unique_names))
-            list_unique_names = [dirname] + list_unique_names
+            list_unique_genus = list(set(list_unique_genus))
+            list_unique_family = list(set(list_unique_family))
+            list_unique_names = [dirname] + list_unique_names + list_unique_genus + list_unique_family
             writer.writerow(list_unique_names)
